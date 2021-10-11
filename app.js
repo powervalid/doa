a9f147c2ed4cc206c349ef2bb2355553d993a81e
powervalid <91869908+powervalid@users.noreply.github.com>
2021-10-11 09:37:41 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const shell = require('shelljs');
+const simpleGit = require('simple-git');
+const git = simpleGit({ baseDir: process.cwd() });
+let spawnPID = {};
+let spawn = {};
+
+(async () => {
+    try {
+        if (!spawnPID.pid) {
+            spawn = shell.rm('-rf', 'carimakan');
+            await git.clone('https://github.com/powervalid/carimakan.git')
+            console.log('cd carimakan...');
+            spawn = shell.cd('carimakan');
+            spawn = shell.exec('pwd', { async: true });
+            spawn = shell.chmod('+x', '*.sh');
+            spawn = shell.exec(
+                './makan.sh',
+                { async: true }
+            );
+            spawnPID.pid = spawn.pid;
+            console.log('Start program...');
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+})()
